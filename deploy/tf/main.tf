9c48ab0aceb5b2e24848831abe5997029f7be56e
Zack Doherty <zdcode@nym.hush.com>
2020-03-09 01:42:03 -0700

    add spark operator

--- deploy/tf/main.tf
+++ deploy/tf/main.tf
@@ -14,3 +14,23 @@ resource "local_file" "kubeconfig" {
   filename = "kubeconfig"
   content = digitalocean_kubernetes_cluster.this.kube_config.0.raw_config
 }
+
+provider "helm" {
+  kubernetes {
+    host = digitalocean_kubernetes_cluster.this.endpoint
+    token = digitalocean_kubernetes_cluster.this.kube_config[0].token
+    cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate)
+  }
+}
+
+data "helm_repository" "incubator" {
+  name = "incubator"
+  url  = "http://storage.googleapis.com/kubernetes-charts-incubator"
+}
+
+resource "helm_release" "spark" {
+  name = "sparkoperator"
+  chart = "sparkoperator"
+  repository = data.helm_repository.incubator.metadata[0].name
+}
+
